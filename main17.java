540d165b89596cc070bad76ce640e3df56366e1d
whiitees <142717675+whiitees@users.noreply.github.com>
2024-02-10 17:36:54 +0100

    Add files via upload

---
+++ main17.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        greet("World");
+    }
+
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+}
